e788b6490060432d41070700f0d16fdf92618f3a
joviankartik0 <jovian.kartiko@student.divd.nl>
2023-03-10 14:27:18 +0100

    papi-gelato html en css toegevoegd

---
+++ js/papi-gelato.js
@@ -0,0 +1,25 @@
+lijstHornOfBak = ["hoorntje", "bakje"]
+lijstToppings = []
+
+
+
+
+
+
+ijs = {
+    bolletjes:0,
+    hoorntjes:0,
+    bakjes:0,
+    smaak:"",
+}
+
+let ijsje = new Object();
+ijsje["bolletjes"] = 3;
+console.log(ijsje);
+
+let bakjes = 1
+let lijst = [];
+for (x=0;x<5;x++){
+    lijst.push({"bolletjes":x, "bakjes":bakjes})
+}
+console.log(lijst);

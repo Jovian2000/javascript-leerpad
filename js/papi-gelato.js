b8048960158d6892cd50bc9ebb2b60fef6d0cd3f
joviankartik0 <jovian.kartiko@student.divd.nl>
2023-03-13 15:13:35 +0100

    update papi-gelato html/css/js

--- js/papi-gelato.js
+++ js/papi-gelato.js
@@ -1,25 +1,69 @@
-lijstHornOfBak = ["hoorntje", "bakje"]
-lijstToppings = []
+const prijsBolletje = 0.95 
+const prijsHoorn = 1.25
+const prijsBakje = 0.75
+const prijsSlagroom = 0.50
+const prijsSprinkels = 0.30
+const prijsCaramelSausHorn = 0.60
+const prijsCaramelSausBak = 0.90
 
-
-
-
-
-
-ijs = {
-    bolletjes:0,
-    hoorntjes:0,
-    bakjes:0,
-    smaak:"",
+var nummerIjs = 0
+var nieuwIjs = true
+var arrayIjsjes = []
+var ijs = {
+    "bolletjes (chocola)": 0,
+    "bolletjes (vanille)": 0,
+    "bolletjes (aardbei)": 0,
+    "hoorn of bak": "",
+    "topping": "",
 }
 
-let ijsje = new Object();
-ijsje["bolletjes"] = 3;
-console.log(ijsje);
+function toevoegingenSmaak(item) {
+    // als de gebruiker een nieuw ijsje wilt, dan maakt hij een object aan die hij vervolgens gelijk in de lijst stop en daarna gaat hij gelijk toevoegen welke smaak je had gekozen.
+    if (nieuwIjs) {
+        arrayIjsjes.push({
+            "bolletjes (chocola)": 0,
+            "bolletjes (vanille)": 0,
+            "bolletjes (aardbei)": 0,
+            "hoorn of bak": "",
+            "topping": "",
+        })
+        nieuwIjs = false
+    }
 
-let bakjes = 1
-let lijst = [];
-for (x=0;x<5;x++){
-    lijst.push({"bolletjes":x, "bakjes":bakjes})
+    // hier kijkt hij welke smaak is doorgegeven en doet 1 bij de waarde erbij
+    if (item == "choco") {
+        arrayIjsjes[nummerIjs]["bolletjes (chocola)"] += 1 
+    } else if (item == "vanille") {
+        arrayIjsjes[nummerIjs]["bolletjes (vanille)"] += 1 
+    } else if (item == "aardbei") {
+    arrayIjsjes[nummerIjs]["bolletjes (aardbei)"] += 1 
+    }
 }
-console.log(lijst);
+
+function afronding(){
+    // hier haalt hij de resterende items op die op de website staat
+    let hoornOfBak = document.getElementById("hoorn-of-bak").value
+    let topping = document.getElementById("topping").value
+
+    // hier kijkt hij of je een hoorntje of bakje wilt, daarna zet hij in de object welke je hebt gekozen
+    if (hoornOfBak == "hoorn") {
+        arrayIjsjes[nummerIjs]["hoorn of bak"] = "hoorn"
+    } else if (hoornOfBak == "bak") {
+        arrayIjsjes[nummerIjs]["hoorn of bak"] = "bak"
+    }
+
+    // hier kijkt welke topping je hebt gekozen en zet daarna in wat jij hebt gekozen
+    if (topping == "slagroom") {
+        arrayIjsjes[nummerIjs]["topping"] = "slagroom"
+    } else if (topping == "sprinkels") {
+        arrayIjsjes[nummerIjs]["topping"] = "sprinkels"
+    } else if (topping == "caramel saus") {
+        arrayIjsjes[nummerIjs]["topping"] = "caramel saus"
+    } else {
+        arrayIjsjes[nummerIjs]["topping"] = "geen"
+    }
+
+    nieuwIjs = true
+    nummerIjs += 1
+    console.log(arrayIjsjes)
+}
